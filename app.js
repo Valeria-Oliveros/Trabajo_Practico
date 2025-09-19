486c433d63bf4f6060b4b0e2474b740f0d06d3db
Valeria Oliveros Corredor <161951355+Valeria-Oliveros@users.noreply.github.com>
2025-09-18 23:51:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-ppp
+//------------------------------------------------------------------------------------------------------------
+// Título: Trabajo Práctico Integrador
+// Programa: Gestión simple de biblioteca
+// Autor: Valeria Oliveros - Agostina Campos - Micaela Martinez - Isabel Peréz - Aketzalli Alonso
+// Fecha: 23/09/2025
+// Intitución: Ada ITW
+//------------------------------------------------------------------------------------------------------------
+
+// 1. Estructura de Datos
+// A. Creamos los arrays de los 10 libros que solicita el ejercicio
+let libros = [
+    { id: 1, titulo: "El Quijote", autor: "Miguel de Cervantes", anio: 1605, genero: "Novela", disponible: true },
+    { id: 2, titulo: "El psicoanalista", autor: "John Katzenbach", anio: 2002, genero: "Thriller psicológic0", disponible: true },
+    { id: 3, titulo: "Los Cuatro Acuerdos", autor: "Miguel Ruiz", anio: 2024, genero: "Autoayuda", disponible: true },
+    { id: 4, titulo: "El ladrón del rayo", autor: "Rick Riordan", anio: 2005, genero: "Fantasía", disponible: true },
+    { id: 5, titulo: "Donde no puedas encontrarme", autor: "Tamara Molina", anio: 2024, genero: "Romance", disponible: true },
+    { id: 6, titulo: "It", autor: "Stephen King", anio: 1986, genero: "Terror", disponible: true },
+    { id: 7, titulo: "El resplandor", autor: "Stephen King", anio: 1977, genero: "Terror", disponible: true },
+    { id: 8, titulo: "Harry potter y la piedra filosofal", autor: "J.K. Rowling", anio: 1997, genero: "Fantasía", disponible: true },
+    { id: 9, titulo: "Harry potter y las reliquias de la muerte", autor: "J.K. Rowling", anio: 2007, genero: "Fantasía", disponible: true },
+    { id: 10, titulo: "La paciente sileciosa", autor: "Alex Michaelides", anio: 2019, genero: "Thriller psicológico", disponible: true },
+];
